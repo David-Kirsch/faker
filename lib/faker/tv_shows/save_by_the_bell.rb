7c94f4f0042f12bfa3d65c61e86c49a787034d53
David-Kirsch <davidjkirsch@gmail.com>
2020-08-16 23:11:05 -0400

    Added Save By The Bell class

---
+++ lib/faker/tv_shows/save_by_the_bell.rb
@@ -0,0 +1,23 @@
+module Faker
+    class TvShows
+        class SaveByTheBill < Base
+            flexible :save_by_the_bell
+
+            class << self
+
+                def character
+                    fetch('save_by_the_bell.characters')
+                end
+
+                def quote
+                    fetch('save_by_the_bell.quotes')
+                end
+                
+                def location
+                    fetch('save_by_the_bell.locations')
+                end
+            end
+        end
+    end
+end
+
